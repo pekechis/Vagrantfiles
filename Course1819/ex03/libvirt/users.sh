f69224c2c39e70f1ff5841ac17b331a759fcc259
Juan Diego <pekechis@gmail.com>
2018-10-16 09:56:28 +0200

    Ex03

--- Course1819/ex03/libvirt/users.sh
+++ Course1819/ex03/libvirt/users.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 sudo apt-get -y install whois
 
-clave = "$(mkpasswd 12345)"
+clave=$(mkpasswd 12345)
+echo "$clave"
 
-useradd -d /home/perez -m -p $clave
-useradd -d /home/jimenez -m -p $clave
+sudo useradd -d /home/perez -m perez -p "$clave"
+sudo useradd -d /home/jimenez -m jimenez -p "$clave"
